dce18e154bfbd020db9f4dffc09ec4b3ced65a37
NaveenRaaj5126 <rajumudiraj5126@gmail.com>
2024-01-10 12:17:50 +0530

    Hello
    New commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,32 @@ import '../App.css';
 import { useState } from 'react';
 function App() {
       const [name,setName] = useState('')
-      //to get the entered value
-      const onMynameChange=(e)=>{
+      const [nameError,setNameError] = useState('')
+      const [emailError,setEmailError] =useState('')
+      const [email,setEmail] = useState('')
+  const onMynameChange=(e)=>{
       setName(e.target.value);
-      setError("")
+      setNameError("")
       } 
-      const [error,setError] = useState('')
-
+      const onMyEmailChange =(e)=>{
+        setEmail(e.target.value);
+        setEmailError("")
+      }
       
-     const handleSubmit=()=>{
+  const handleSubmit=()=>{
       if(name==""){
-        setError("Please enter the value")
+        setNameError("Please enter the value")
+      }
+      if(email==""){
+        setEmailError("Enter your email id ")
       }
     else{
-      setError("")
+      setNameError("")
+      setEmailError("")
     }
      }
   return (
-    //Git checking 
+    
     <div className="App">
         <h3>Welcome to Sigma edge solutions</h3>   
         <div>
@@ -29,55 +37,30 @@ function App() {
             type='text'
             value={name}
             onChange={onMynameChange}
-            >
-            </input>
+            />
+          
             <div>
-            <span>{error}</span>
+            <span>{nameError}</span>
             </div>        
         </div> 
+        <div>
+          <div><label>Email</label></div>
+            <input 
+            type='email'
+            value={email}
+            onChange={onMyEmailChange}
+           />
+           
+            <div>
+            <span>{emailError}</span>
+            </div>        
+        </div> 
+        
            
     
     <button 
     onClick={handleSubmit}
      >Submit</button>
-
-      {/* <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="phoneNumber">Phone Number</label>
-          <input
-            type="tel"
-            id="phoneNumber"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form> */}
-
     </div>
 
   );
