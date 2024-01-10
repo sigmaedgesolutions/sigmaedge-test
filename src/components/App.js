c09a7857f99adf0bda45701d2490b71628c25e1f
prasad-ch-96 <prasad.chukkala@metrolabsservices.com>
2024-01-10 12:53:08 +0530

    Added h2 tag
    with description

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,58 @@
-
-import '../App.css';
-import { useState } from 'react';
+import "../App.css";
+import { useState } from "react";
 function App() {
-      const [name,setName] = useState('')
-      const [nameError,setNameError] = useState('')
-      const [emailError,setEmailError] =useState('')
-      const [email,setEmail] = useState('')
-  const onMynameChange=(e)=>{
-      setName(e.target.value);
-      setNameError("")
-      } 
-      const onMyEmailChange =(e)=>{
-        setEmail(e.target.value);
-        setEmailError("")
-      }
-      
-  const handleSubmit=()=>{
-      if(name==""){
-        setNameError("Please enter the value")
-      }
-      if(email==""){
-        setEmailError("Enter your email id ")
-      }
-    else{
-      setNameError("")
-      setEmailError("")
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [email, setEmail] = useState("");
+  const onMynameChange = (e) => {
+    setName(e.target.value);
+    setNameError("");
+  };
+  const onMyEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
+  };
+
+  const handleSubmit = () => {
+    if (name == "") {
+      setNameError("Please enter the value");
     }
-     }
+    if (email == "") {
+      setEmailError("Enter your email id ");
+    } else {
+      setNameError("");
+      setEmailError("");
+    }
+  };
   return (
-    
     <div className="App">
-        <h3>Welcome to Sigma edge solutions</h3>   
+      <h3>Welcome to Sigma edge solutions</h3>
+      <div>
         <div>
-          <div><label>Name</label></div>
-            <input 
-            type='text'
-            value={name}
-            onChange={onMynameChange}
-            />
-          
-            <div>
-            <span>{nameError}</span>
-            </div>        
-        </div> 
-        <div>
-          <div><label>Email</label></div>
-            <input 
-            type='email'
-            value={email}
-            onChange={onMyEmailChange}
-           />
-           
-            <div>
-            <span>{emailError}</span>
-            </div>        
-        </div> 
-    <button 
-    onClick={handleSubmit}
-     >Submit</button>
+          <label>Name</label>
+        </div>
+        <input type="text" value={name} onChange={onMynameChange} />
 
-     {/* Prasad You have to work from here */}
+        <div>
+          <span>{nameError}</span>
+        </div>
+      </div>
+      <div>
+        <div>
+          <label>Email</label>
+        </div>
+        <input type="email" value={email} onChange={onMyEmailChange} />
+
+        <div>
+          <span>{emailError}</span>
+        </div>
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+
+      {/* Prasad You have to work from here */}
+      <h2>Here is my work</h2>
     </div>
-
   );
 }
 
