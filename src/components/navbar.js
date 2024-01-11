72524de7d485a1df60c1443a0b3cd450180020ba
NaveenRaaj5126 <rajumudiraj5126@gmail.com>
2024-01-11 16:07:35 +0530

    Updates checking
    Description

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,8 @@ import {logo} from "../images/"
 function Navebar (){
     return(
         <div>
-            
+            <textarea></textarea>
+            <p>Hello world</p>
         </div>
     )
 }
